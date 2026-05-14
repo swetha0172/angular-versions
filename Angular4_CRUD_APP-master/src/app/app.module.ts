fd1b54c9381d48cc6e2ddfcd8dbcdd63bf3d8848
swetha0172 <S.Swetha2@ltm.com>
2026-05-14 16:59:18 +0000

    version 8

--- Angular4_CRUD_APP-master/src/app/app.module.ts
+++ Angular4_CRUD_APP-master/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -28,7 +28,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [DataService],
--- Angular4_CRUD_APP-master/src/app/services/data.service.ts
+++ Angular4_CRUD_APP-master/src/app/services/data.service.ts
@@ -1,33 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class DataService {
 
-    constructor(public http: Http) {
+    constructor(public http: HttpClient) {
     }
 
     private readonly baseUrl = 'http://localhost:3000/users';
 
-    getUsers() {
-        return this.http.get(this.baseUrl)
-        .map(res => res.json());
+    getUsers(): Observable<any> {
+        return this.http.get(this.baseUrl);
     }
 
-    addUser(user: any) {
-        return this.http.post(this.baseUrl, user)
-        .map(res => res.json());
+    addUser(user: any): Observable<any> {
+        return this.http.post(this.baseUrl, user);
     }
 
-    deleteUser(id: any) {
-        return this.http.delete(this.baseUrl + '/' + id)
-        .map(res => res.json());
+    deleteUser(id: any): Observable<any> {
+        return this.http.delete(`${this.baseUrl}/${id}`);
     }
 
-    updateUser(user: any) {
-        return this.http.put(this.baseUrl + '/' + user.id, user)
-        .map(res => res.json());
+    updateUser(user: any): Observable<any> {
+        return this.http.put(`${this.baseUrl}/${user.id}`, user);
     }
 }
 
